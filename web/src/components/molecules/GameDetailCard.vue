2860b804c72a1498bd5660b94f34678ee48d88a3
Mahito Fujino <kerukukku1@gmail.com>
2020-03-21 13:37:00 +0900

    add game card component

---
+++ web/src/components/molecules/GameDetailCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
+        <div>
+            <span>現在の対局数 : {{ gameCount }}</span>
+            <span>部屋主 : {{ ownerName }}</span>
+            <span>作成日 : {{ createdDate }}</span>
+        </div>
+        <button>
+            Join
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        ownerName: String,
+        description: String,
+        roomId: Number,
+        gameCount: Number,
+        createdDate: Date,
+    }
+}
+</script>
+
+<style>
+
+</style>
